3349393eafca0f373995ed845c347424e0e77458
Cathy <cxzhu@usc.edu>
2023-09-19 18:15:28 -0700

    pickle code

--- ee250/lab04/pickle_test.py
+++ ee250/lab04/pickle_test.py
@@ -2,14 +2,19 @@ import pickle
 
 notes = []
 
-# TODO: Using the pickle module...
-
 # A. If notes.pickle exists, read it in using pickle and assign the content to
 #   the notes variable
+with open('mail.pickle', 'rb') as contents:
+    notes = pickle.load(contents)
 
 # B. Print out notes
+print(notes)
 
 # C. Read in a string from the user using input() and append it to notes
+userInput = input("Write a string to add to notes: ")
+notes.append(userInput)
 
 # D. Save notes to notes.pickle
+with open('mail.pickle', 'wb') as contents:
+    pickle.dump(notes, contents, protocol = pickle.HIGHEST_PROTOCOL)
 
